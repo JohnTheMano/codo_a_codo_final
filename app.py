bb6e2ad1189ec68e4f0498729bd17282fd32206d
JohnTheMano <101668961+JohnTheMano@users.noreply.github.com>
2023-07-07 22:37:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
-from flask import Flask ,jsonify ,request
+# from flask import Flask ,jsonify ,request
 # del modulo flask importar la clase Flask y los métodos jsonify,request
-from flask_cors import CORS       # del modulo flask_cors importar CORS
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-app=Flask(__name__)  # crear el objeto app de la clase Flask
-CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
+# from flask_cors import CORS       # del modulo flask_cors importar CORS
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_marshmallow import Marshmallow
+# app=Flask(__name__)  # crear el objeto app de la clase Flask
+# CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
 
 
 # configuro la base de datos, con el nombre el usuario y la clave
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
+# app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
 # URI de la BBDD                          driver de la BD  user:clave@URLBBDD/nombreBBDD
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
-db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
-ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
+# db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
+# ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
 
 
 # defino la tabla
-class Producto(db.Model):   # la clase Producto hereda de db.Model    
-    id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
-    nombre=db.Column(db.String(100))
-    precio=db.Column(db.Integer)
-    stock=db.Column(db.Integer)
-    imagen=db.Column(db.String(400))
-    def __init__(self,nombre,precio,stock,imagen):   #crea el  constructor de la clase
-        self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
-        self.precio=precio
-        self.stock=stock
-        self.imagen=imagen
+# class Producto(db.Model):   # la clase Producto hereda de db.Model    
+ #   id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
+ #   nombre=db.Column(db.String(100))
+ #   precio=db.Column(db.Integer)
+ #   stock=db.Column(db.Integer)
+ #   imagen=db.Column(db.String(400))
+ #   def __init__(self,nombre,precio,stock,imagen):   #crea el  constructor de la clase
+ #       self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
+ #       self.precio=precio
+ #       self.stock=stock
+ #       self.imagen=imagen
 
 
 
